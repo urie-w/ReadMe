816639e6c8e04da2e8bb7e8a5734dfc44073f554
urie-w <urieasia@gmail.com>
2024-08-04 12:49:40 -0600

    license badges

--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -1,14 +1,26 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
-
-// TODO: Create a function that returns the license link
-// If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseBadge(license) {
+  if (license === 'MIT') {
+      return '![License: MIT](https://img.shields.io/badge/license-MIT-yellow.svg)';
+   } else if (license === 'Apache') {
+        return '![License](https://img.shields.io/badge/License-Apache%202.0-blue.svg)';
+      } else if(license === 'BSD') {
+        return '![License](https://img.shields.io/badge/License-BSD%203--Clause-blue.svg)';
+        // TODO: Create a function that returns the license link
+        // If there is no license, return an empty string
+      } else {
+        return '';
+      }
+    }
+    
+    //function renderLicenseLink(license) {}
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function renderLicenseSection(license) {
+  
+}
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
